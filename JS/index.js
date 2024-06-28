1226cf24a13f0b93e3d8d2921e29d7d564ed64a6
mahbahig <mahbahig2@gmail.com>
2024-06-28 12:12:56 +0300

    First commit

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+// let x = (username) => { console.log('Function x '+username); }
+// x('mahmoud');
+
+// function y(username) {
+//     console.log('Function y ' + username);
+// }
+// y('mahmoud')
+
+// z = function (username) {
+//     console.log('Function z ' + username);
+// }
+// z('mahmoud')
+
+function trylol (callback) {
+    callback()
+}
+trylol(function(){console.log('hello from mahmoud')})
