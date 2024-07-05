f3a7fbf706c2f514d47e1b96950cad24ae596ae5
mahbahig <mahbahig2@gmail.com>
2024-07-05 13:05:19 +0300

    Finished HTML and CSS

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,1 @@
-// let x = (username) => { console.log('Function x '+username); }
-// x('mahmoud');
-
-// function y(username) {
-//     console.log('Function y ' + username);
-// }
-// y('mahmoud')
-
-// z = function (username) {
-//     console.log('Function z ' + username);
-// }
-// z('mahmoud')
-
-function trylol (callback) {
-    callback()
-}
-trylol(function(){console.log('hello from mahmoud')})
+const key = `ffbffdcda0644538b3d113835240407`;
